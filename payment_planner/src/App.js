acb3f355653e6927a7ac987d85eaf7774a4c1c45
Kamila <kamilagrzadko@gmail.com>
2023-07-13 15:00:09 +0200

    main table layout

--- payment_planner/src/App.js
+++ payment_planner/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { SettingsIcon } from '@chakra-ui/icons'
-import { ChakraProvider, Checkbox, Button, Flex } from '@chakra-ui/react'
+import background from "./components/img/background.jpg"
+import { SunIcon } from '@chakra-ui/icons'
+import { ChakraProvider, Checkbox, Button, Flex, Heading, Center, Stack } from '@chakra-ui/react'
 import {
   Table,
   Thead,
@@ -12,18 +13,34 @@ import {
   TableContainer,
 } from '@chakra-ui/react'
 
+const MainBackground = styled.div`
+  position: absolute;
+  min-height: 100vh;
+  width: 100%;
+  background-image: url(${background});
+
+`
 function App() {
   return (
+    <MainBackground>
     <ChakraProvider>
-    <TableContainer>
+      <Center>
+    <TableContainer w='80%'>
     <Flex justifyContent="end">
-    <Button colorScheme='blue'>Login<SettingsIcon/></Button>
+    
+    <Button colorScheme='teal' rightIcon={<SunIcon/>} m={4} borderRadius='0px'>Login</Button>
+
   </Flex>
-  <Table variant='striped' colorScheme='teal'>
+  <Center>
+    <Heading mb={4}>Monthly payment planner</Heading>
+  </Center>
+    
+  <Center>
+  <Table size="lg" variant='striped' colorScheme='cyan' w='60%'>
     
     <Thead>
       <Tr>
-        <Th>Date to pay due bills</Th>
+        <Th>Day to pay due bills</Th>
         <Th>Bills</Th>
         <Th>Jan</Th>
         <Th>Feb</Th>
@@ -41,29 +58,31 @@ function App() {
     </Thead>
     <Tbody>
       <Tr>
-        <Td>10</Td>
-        <Td>Czynsz</Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
-        <Td ><Checkbox isInvalid/></Td>
+        <Td color="gray" fontWeight='bold'>10</Td>
+        <Td color="gray" fontWeight='bold'>Czynsz</Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
+        <Td ><Checkbox colorScheme="teal" borderColor="teal"/></Td>
       </Tr>
     </Tbody>
   </Table>
+  </Center>
   <Flex justifyContent="end">
-    <Button colorScheme='blue'>Save</Button>
+    <Button colorScheme='teal' px={8} m={4} borderRadius='0px'>Save</Button>
   </Flex>
 </TableContainer>
+</Center>
 </ChakraProvider>
-    
+</MainBackground>
   );
 }
 
